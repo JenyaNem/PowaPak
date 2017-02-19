f73b99d0c7adcf4e51579856b6d36471e3a5d2bd
Zhenya Nemerovchenko <nemerov4enko@gmail.com>
2017-02-19 13:47:21 +0200

    added percentage preloader

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
+var countImages = $('#main_animation-hide img').size();
+
+$('#main_animation-hide').imagesLoaded()
+.always( function( instance ) {
+  $('#progress-bar').fadeOut(function() {
+    $('#main_animation-hide').fadeIn();
+  });
+})
+.progress( function( instance, image ) {
+  if(image.isLoaded) {
+    $(image.img).addClass('loaded');
+
+    var countLoadedImages = $('#main_animation-hide img.loaded').size();
+
+    var width = new Number(100 * (countLoadedImages / countImages));
+    width = width.toFixed(2) + '%';
+
+    // $('#progress-bar > #progress').css({
+    //   'width' : width
+    // });
+
+    $('#progress-bar > p').html(width);
+  }
+});
+
 $(function() {
 
-	
+
 
 });
